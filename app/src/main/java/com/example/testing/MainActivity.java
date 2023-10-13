4804653c2aafc198c697dd069321817bf1a33cad
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-10-13 11:05:14 -0600

    buttons work except the ones like add, multiply, etc

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -7,13 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText resultado;
-    private StringBuilder input;
+import com.google.android.material.button.MaterialButton;
 
-    TextView numbers, operacion;
-    Button button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8,button_9,
+public class MainActivity extends AppCompatActivity {
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+    private char currentSymbol;
+    private DecimalFormat decimalFormat;
+    private TextView operacion, numbers;
+    private double firstValue = Double.NaN;
+    private double secondValue;
+    private MaterialButton btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,
             button_sum, button_res, button_div, button_mul, button_clear, button_fin;
 
     @Override
@@ -21,35 +29,170 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultado = findViewById(R.id.numbers);
-        input = new StringBuilder();
+        operacion = findViewById(R.id.oper);
+        numbers = findViewById(R.id.numbers);
+
+        btn0 = findViewById(R.id.button_0);
+        btn1 = findViewById(R.id.button_1);
+        btn2 = findViewById(R.id.button_2);
+        btn3 = findViewById(R.id.button_3);
+        btn4 = findViewById(R.id.button_4);
+        btn5 = findViewById(R.id.button_5);
+        btn6 = findViewById(R.id.button_6);
+        btn7 = findViewById(R.id.button_7);
+        btn8 = findViewById(R.id.button_8);
+        btn9 = findViewById(R.id.button_9);
+
+        button_sum = findViewById(R.id.button_sum);
+        button_res = findViewById(R.id.button_res);
+        button_div = findViewById(R.id.button_div);
+        button_mul = findViewById(R.id.button_mul);
+        button_clear = findViewById(R.id.button_clear);
+        button_fin = findViewById(R.id.button_fin);
 
         // Set up click listeners for buttons
-        setupButtonClickListeners();
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "0");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacion.setText(operacion.getText() + "9");
+            }
+        });
+        button_sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allCalculations();
+                currentSymbol = ADDITION;
+                numbers.setText(decimalFormat.format(firstValue) + "+");
+                operacion.setText(null);
+            }
+        });
+        button_res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allCalculations();
+                currentSymbol = SUBTRACTION;
+                numbers.setText(decimalFormat.format(firstValue) + "-");
+                operacion.setText(null);
+            }
+        });
+        button_mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allCalculations();
+                currentSymbol = MULTIPLICATION;
+                numbers.setText(decimalFormat.format(firstValue) + "x");
+                operacion.setText(null);
+            }
+        });
+        button_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allCalculations();
+                currentSymbol = DIVISION;
+                numbers.setText(decimalFormat.format(firstValue) + "/");
+                operacion.setText(null);
+            }
+        });
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (operacion.getText().length() > 0) {
+                    CharSequence currentText = operacion.getText();
+                    operacion.setText(currentText.subSequence(0, currentText.length() - 1));
+                } else {
+                    firstValue = Double.NaN;
+                    secondValue = Double.NaN;
+                    operacion.setText("");
+                    numbers.setText("");
+                }
+            }
+        });
+        button_fin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allCalculations();
+                numbers.setText(decimalFormat.format(firstValue));
+                firstValue = Double.NaN;
+                currentSymbol = '0';
+            }
+        });
     }
 
-    private void setupButtonClickListeners() {
-        // Find and set click listeners for digit buttons (0-9)
-        for (int i = 0; i <= 9; i++) {
-            int resId = getResources().getIdentifier("button" + i, "id", getPackageName());
-            Button button = findViewById(resId);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Button clickedButton = (Button) view;
-                    input.append(clickedButton.getText().toString());
-                    resultado.setText(input.toString());
-                }
-            });
+    private void allCalculations(){
+        if (!Double.isNaN(firstValue)) {
+            secondValue = Double.parseDouble(operacion.getText().toString());
+            operacion.setText(null);
+            if (currentSymbol == ADDITION)
+                firstValue = this.firstValue + secondValue;
+            else if (currentSymbol == SUBTRACTION)
+                firstValue = this.firstValue - secondValue;
+            else if (currentSymbol == MULTIPLICATION)
+                firstValue = this.firstValue * secondValue;
+            else if (currentSymbol == DIVISION)
+                firstValue = this.firstValue / secondValue;
+        } else {
+            try {
+                firstValue = Double.parseDouble(operacion.getText().toString());
+            } catch (Exception e) {
+            }
         }
     }
 
 
 
-    @Override
-    public void onClick(View view) {
-        Button btn = (Button) view;
-        String btnText = btn.getText().toString();
-        operacion.setText(btnText);
-    }
+
 }
